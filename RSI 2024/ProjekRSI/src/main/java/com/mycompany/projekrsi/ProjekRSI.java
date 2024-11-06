32fba27cd2f4b2a123c911541dc3f3b7548c6740
KhansaFathonah <khansafathonahf@gmail.com>
2024-11-06 15:30:17 +0700

    Update ProjekRSI.java

--- RSI 2024/ProjekRSI/src/main/java/com/mycompany/projekrsi/ProjekRSI.java
+++ RSI 2024/ProjekRSI/src/main/java/com/mycompany/projekrsi/ProjekRSI.java
@@ -16,6 +16,10 @@ public class ProjekRSI {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
         
         System.out.println("hihiii");
     }
